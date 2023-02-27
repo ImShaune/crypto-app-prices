981a7c262912268810591b65f4cd9bc1df00c926
ImShaune <nndesigner@outlook.es>
2023-02-27 11:00:20 -0300

    coinsRowUpdate

--- src/components/CoinRow.js
+++ src/components/CoinRow.js
@@ -7,7 +7,7 @@ const CoinRow = ({coin, index}) => {
     <tr>
         <td>{index}</td>
         <td>
-            <img src = {coin.image} alt={coin.name} style={{width: '3%'}} className= 'img-fluid me-4' />
+            <img src = {coin.image} alt={coin.name} style={{width: '15%'}} className= 'img-fluid me-4' />
             <span>{coin.name}</span>
             <span className='ms-3 text-muted text-uppercase'>{coin.symbol}</span>
         </td>
